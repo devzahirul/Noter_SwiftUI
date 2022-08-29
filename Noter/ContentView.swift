4d9729b75c3906bc304771ccf2cde086451bb1d2
Zahirul_curlybracers <zahirulislam@curlybracers.com>
2022-08-29 20:14:59 +0600

    added

---
+++ Noter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Noter
+//
+//  Created by Islam Md. Zahirul on 29/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Noter/NoterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoterApp.swift
+//  Noter
+//
+//  Created by Islam Md. Zahirul on 29/8/22.
+//
+
+import SwiftUI
+
+@main
+struct NoterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
